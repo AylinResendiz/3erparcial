3b8c7b796c8832143a2b8f5ac834f71d1baf3bb2
Aylin <aylin.resendiz.cbtis03@gmail.com>
2024-05-22 17:32:27 -0600

    trabajo cinco

---
+++ trabajo5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char primis,ultima;
+    cout<<"introduzca su primera y ultima inicial\n";
+    cin>>primis;
+    cin>>ultima;
+    cout<<"HOLAA "<<primis<<ultima<<endl;
+    return 0;
+}
