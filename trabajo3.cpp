71c88b233f5603cdb45fa7508c1ff46603a8f194
Aylin <aylin.resendiz.cbtis03@gmail.com>
2024-05-22 17:10:53 -0600

    trabajo tres

---
+++ trabajo3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,resul;
+    cin>>n1;
+    cin>>n2;
+    resul=n1+n2;
+    cout<<"el resultado es: "<<resul<<endl;
+    return 0;
+}
