0466d23d4d1fd9d7e10539143c2036efceea14b5
Aylin <aylin.resendiz.cbtis03@gmail.com>
2024-05-23 10:58:43 -0600

    el tamaño de las variables...

---
+++ trabajo7.cpp
@@ -0,0 +1,10 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+    cout<<"el tamaño de variables de coma flotante es: "<<sizeof(float)<<endl;
+    cout<<"bytes"<<endl;
+    cout<<"el tamaño de variables de doble presicion es: "<<sizeof(double);
+    cout<<"bytes"<<endl;
+    return 0;
+}
