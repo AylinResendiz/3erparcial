f2bc488ab4256eb4f6f790d07a9cb31c9c48666b
Aylin <aylin.resendiz.cbtis03@gmail.com>
2024-05-23 10:28:07 -0600

    Trabajo 6 :)

---
+++ trabajo6.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+#define T "tambor de hojalata"
+using namespace std;
+int main ()
+{
+  char st[21]="todo puede hacerse";
+  puts(T);
+  puts("permiso para salir en la foto");
+  puts(st);
+  puts(&st[8]);
+  return 0;
+}
