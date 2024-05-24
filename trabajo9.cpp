a5eabaab37199d2d353b2a259b8381f804536a8b
Aylin <aylin.resendiz.cbtis03@gmail.com>
2024-05-24 16:04:34 -0600

    trabalo 9 :D

---
+++ trabajo9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m=45,n=75;
+    cout<<"m="<<m; cout<<" n="<<n<<endl;
+    ++m;
+    n--;
+    cout<<"m="<<m; cout<<" n="<<n<<endl;
+}
