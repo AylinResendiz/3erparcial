c985e2fec9a4e754f3141f27cdee6e7037abfa26
Aylin <aylin.resendiz.cbtis03@gmail.com>
2024-05-24 16:51:28 -0600

    pide ingresar 3 numeros enteros para dar acceso

---
+++ trabajo12.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+    int n1,n2,n3;
+    cout<<"ingrese 3 numeros enteros de una cifra"<<endl;
+    cin>>n1; cin>>n2; cin>>n3;
+    if (n1==1 && n2==2 && n3==3)
+    {
+        cout<<"ACCESO PERMITIDO"<<endl;
+    }
+    return 0;
+}
