38d019f6d370ab9ad16156991e0b540a267c4965
Aylin <aylin.resendiz.cbtis03@gmail.com>
2024-06-03 12:19:59 -0600

    el 14 pero con while

---
+++ trabajo15.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i=1,mayor,menor,sum;
+    cout<<"ingrese 10 numeros enteros"<<endl;
+    while (i<=10)
+    {
+        i++;
+        cin>>n;
+        if (n>mayor)
+        {
+            mayor=n;
+        }
+        else if (n<menor)
+        {
+            menor=n;
+        }
+        sum=mayor+menor;
+    }
+    cout<<"el numero mayor es: "<<mayor<<endl;
+    cout<<"el numero menor es: "<<menor<<endl;
+    cout<<"la suma de el menor y mayor es: "<<sum<<endl;
+    return 0;
+}
